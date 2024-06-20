d48994e92a02c4c29c4a7d607646390fb5ecb82a
csantos18 <csantos18@oreillyauto.com>
2024-06-20 11:01:40 -0500

    init commit, maybe working?

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
+"use strict";
 import { logger } from 'log';
 
-const teamName = 'Hackathon Team 2';
+const teamName = 'Hackathon Team 3';
+var errorMsg = "";
+const latAndLong = {};
+const watching = "WE'RE WATCHING YOU.";
+const googleMaps = '<iframe \n' +
+    '  width="300" \n' +
+    '  height="170" \n' +
+    '  frameborder="0" \n' +
+    '  scrolling="no" \n' +
+    '  marginheight="0" \n' +
+    '  marginwidth="0" \n' +
+    '  src="https://maps.google.com/maps?q=\'' + latAndLong.lat + '\',\'' + latAndLong.long + '\'&hl=es&z=14&amp;output=embed"\n' +
+    ' >\n' +
+    ' </iframe>\n' +
+    ' <br />\n' +
+    ' <small>\n' +
+    '   <a \n' +
+    '    href="https://maps.google.com/maps?q=\'' + latAndLong.lat + '\',\'' + latAndLong.long + '\'&hl=es;z=14&amp;output=embed" \n' +
+    '    style="color:#0000FF;text-align:left" \n' +
+    '    target="_blank"\n' +
+    '   >\n' +
+    '     See map bigger\n' +
+    '   </a>\n' +
+    ' </small>';
+
+
+
+function getLatAndLong() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            latAndLong.lat = position.coords.latitude;
+            latAndLong.long = position.coords.longitude;
+            return true
+        })
+    } else {
+        errorMsg = "<h1 style='color: #F00'>Geolocation is not supported by this browser.</h1>";
+        return false;
+    }
+}
 
 export function onClientRequest(request) {
     // Outputs a message to the X-Akamai-EdgeWorker-onClientRequest-Log header.
     logger.log('Responding with hello world from the path: %s', request.path);
-    request.respondWith(200, {}, `Hello World From ${teamName}`);
+    if (getLatAndLong()) {
+        request.respondWith(200, {}, watching + googleMaps);
+    } else {
+        request.respondWith(400, {}, errorMsg);
+    }
 }
 
 export function onClientResponse(request, response) {
